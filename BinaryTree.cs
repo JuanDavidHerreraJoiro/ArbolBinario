86d0b8f85bc80c41e8474d6ec231d1a6a977488c
Juan Herrera Joiro <juandavidherrera@unicesar.edu.co>
2023-05-30 13:31:35 -0500

    Commit ArbolBinario

---
+++ BinaryTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolBinario
+{
+    public class BinaryTree
+    {
+        public Node nodehijo { get; set; }
+
+        public Node Ancestor(int value1, int value2)
+        {
+            return Ancestor(nodehijo, value1, value2);
+        }
+
+        private Node Ancestor(Node node, int value1, int value2)
+        {
+            
+            if (node == null)
+            {
+                return null;
+            }
+
+            //Buscamos al node padre de cada hijo
+            if (node.Value == value1 || node.Value == value2)
+            {
+                return node;
+            }
+
+            //Buscamos si tiene mas hijos
+            Node leftLCA = Ancestor(node.Left, value1, value2);
+            Node rightLCA = Ancestor(node.Right, value1, value2);
+
+            //Comparamos si no tiene mas hijos el nodo padre
+            if (leftLCA != null && rightLCA != null)
+            {
+                return node;
+            }
+
+            //Retornamos al ancestro
+            if (leftLCA != null)
+            {
+                return (Node?)leftLCA;
+            }
+            else
+            {
+                return rightLCA;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ArbolBinario;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Crear el arbol
+        BinaryTree tree = new BinaryTree();
+        //1 nivel
+        tree.nodehijo = new Node(70);
+        //2 nivel
+        tree.nodehijo.Left = new Node(49);
+        tree.nodehijo.Right = new Node(84);
+        //3 nivel
+        tree.nodehijo.Left.Left = new Node(37);
+        tree.nodehijo.Left.Right = new Node(54);
+        tree.nodehijo.Right.Left = new Node(78);
+        tree.nodehijo.Right.Right = new Node(85);
+        //4 nivel
+        tree.nodehijo.Left.Left.Left = new Node(22);
+        tree.nodehijo.Left.Left.Right = new Node(40);
+        tree.nodehijo.Left.Right.Left = new Node(51);
+        tree.nodehijo.Right.Left.Left = new Node(76);
+        tree.nodehijo.Right.Left.Right = new Node(80);
+
+        // Busqueda del ancestro
+        int nodeValue1 = 40;
+        int nodeValue2 = 78;
+        Node lcaNode = tree.Ancestor(nodeValue1, nodeValue2);
+        Console.WriteLine($"Ancestro ({nodeValue1}, {nodeValue2}) = {lcaNode.Value}");
+
+        nodeValue1 = 51;
+        nodeValue2 = 37;
+        lcaNode = tree.Ancestor(nodeValue1, nodeValue2);
+        Console.WriteLine($"Ancestro ({nodeValue1}, {nodeValue2}) = {lcaNode.Value}");
+
+        nodeValue1 = 76;
+        nodeValue2 = 85;
+        lcaNode = tree.Ancestor(nodeValue1, nodeValue2);
+        Console.WriteLine($"Ancestro ({nodeValue1}, {nodeValue2}) = {lcaNode.Value}");
+    }
+}
